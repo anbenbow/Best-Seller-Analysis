584c992e99df96b5b9f64e3dac48d13700322bb0
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-21 19:03:35 -0400

    Created lambda print functions for evaluations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from book import Book
 from data import data_list
+from collections import Counter
 
 def data_list_creator(data_list):
     final_data_list = []
@@ -9,14 +10,27 @@ def data_list_creator(data_list):
     return final_data_list    
 
 new_data_list = data_list_creator(data_list)
-#five_moons_or_less = list(filter(lambda book: book.moons ,= 5, new_data_list))
-# for book in five_moons_or_less:
-#     print(f'The planet {book.name} has {str(book.moons)} moons!')
+
+# top_fifty_most_years = list(filter(lambda data: data.id <= 50, new_data_list))
+# for data in top_fifty_most_years:
+#     print(f'The book {data.name} has been in the top 50 for {str(data.moons)} years!')
 
 # sorted_data = sorted(new_data_list,key=lambda book: book.name)
 # for data in sorted_data:
 #      print(data.name)
 #             OR
-sorted_data = sorted(new_data_list,key=lambda book: book.name, reverse=True)
+sorted_data = sorted(new_data_list,key=lambda book: book.user_rating, reverse=True)
 for data in sorted_data:
-    print(data.name)
+    print( )
+# most_common = counter(data_list).most_common()
+# print(data for data in data_list.most_common for data in ['years'])
+
+
+book_for_most_years = lambda : print('This book has been in the top 50s list for the most years: ')
+# result
+genre_the_most = lambda : print('This genre has appeared the most in the top 50s list: ')
+#result
+author_the_most = lambda : print('This author has shown up on the top 50s list the most:')
+#result
+books_for_each_year = lambda : print('These are the top books for each year:')
+#result
