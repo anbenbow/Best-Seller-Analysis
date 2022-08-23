a087472b4c9e06f0d10ac69ef833756d906fa8df
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-08-22 20:15:31 -0400

    Completed evaluation question to determine book with most appearances

--- book.py
+++ book.py
@@ -1,11 +1,11 @@
 class Book:
-    def __init__(self, data_list):
-        self.id = data_list ['id']
-        self.name = data_list ['name']
-        self.author = data_list ['author']
-        self.user_rating = data_list ['user_rating']
-        self.number_of_reviews = data_list ['number_of_reviews']
-        self.price = data_list ['price']
-        self.year = data_list ['year']
-        self.genre = data_list ['genre']
+    def __init__(self, book_dict):
+        self.id =  book_dict ['id']
+        self.name =  book_dict ['name']
+        self.author =  book_dict ['author']
+        self.user_rating =  book_dict ['user_rating']
+        self.number_of_reviews =  book_dict ['number_of_reviews']
+        self.price =  book_dict ['price']
+        self.year =  book_dict ['year']
+        self.genre =  book_dict ['genre']
 
--- main.py
+++ main.py
@@ -1,38 +1,87 @@
+from asyncio import current_task
 from book import Book
 from data import data_list
 from collections import Counter
 
-def data_list_creator(data_list):
-    final_data_list = []
-    for data in data_list:
-        new_data= Book (data)
-        final_data_list.append(new_data)
-    return final_data_list    
 
-new_data_list = data_list_creator(data_list)
+def book_for_most_years (books):
+    current_top_book = {'name':'','count':0}
+    book_names = [book.name for book in books]
+    book_names_distinct = set(book_names)
+    for distinct_book_name in book_names_distinct:
+        list_of_specific_book = [book_name for book_name in book_names if book_name == distinct_book_name]
+        if current_top_book['count'] < len(list_of_specific_book):
+            current_top_book['name'] = distinct_book_name
+            current_top_book['count'] = len(list_of_specific_book)
 
-# top_fifty_most_years = list(filter(lambda data: data.id <= 50, new_data_list))
-# for data in top_fifty_most_years:
-#     print(f'The book {data.name} has been in the top 50 for {str(data.years)} years!')
-
-
-sorted_by_user_rating = sorted(new_data_list,key=lambda book: book.user_rating, reverse=True)
-sorted_by_rating_and_review = sorted(sorted_by_user_rating, key=lambda book: book.number_of_reviews, reverse=True)
-
-c = Counter
-# for data in sorted_by_user_rating:
-#     print(sorted_by_user_rating['name']['year'])
-most_by_rating_and_review = c(sorted_by_rating_and_review.name).most_common()[0]
-print(most_by_rating_and_review)
-# most_common = counter(data_list).most_common()
-# print(data for data in data_list.most_common for data in ['years'])
+    print('This book has been in the top 50s list for the most years: ')
+    print(f"Name: {current_top_book['name']}")
+    print(f"Times in Top 50: {current_top_book['count']}")
 
 
-book_for_most_years = lambda : print('This book has been in the top 50s list for the most years: ')
-# result
-genre_the_most = lambda : print('This genre has appeared the most in the top 50s list: ')
-#result
-author_the_most = lambda : print('This author has shown up on the top 50s list the most:')
-#result
-books_for_each_year = lambda : print('These are the top books for each year:')
-#result
+
+def run():
+    
+    books = []
+    
+    for book_dictionary in data_list:
+        new_book = Book(book_dictionary)
+        books.append(new_book)
+
+    book_for_most_years(books)
+ 
+
+
+
+    
+#    = list(filter(lambda data: data.id <= 50, new_data_list))
+# # for data in top_fifty_most_years:
+# #     print(f'The book {data.name} has been in the top 50 for {str(data.years)} years!')
+
+
+# sorted_by_user_rating = sorted(new_data_list,key=lambda book: book.user_rating, reverse=True)
+# sorted_by_rating_and_review = sorted(sorted_by_user_rating, key=lambda book: book.number_of_reviews, reverse=True)
+
+# print(sorted_by_user_rating)
+
+# c = Counter
+# # for data in sorted_by_user_rating:
+# #     print(sorted_by_user_rating['name']['year'])
+# most_by_rating_and_review = c(sorted_by_rating_and_review.name).most_common()[0]
+# print(most_by_rating_and_review)
+# # most_common = counter(data_list).most_common()
+# # print(data for data in data_list.most_common for data in ['years'])
+
+
+# book_for_most_years = lambda : print('This book has been in the top 50s list for the most years: ')
+
+# # times in top 50: ...
+# genre_the_most = lambda : print('This genre has appeared the most in the top 50s list: ')
+# #genre:
+# # number of appearances:
+# author_the_most = lambda : print('This author has shown up on the top 50s list the most:')
+# #author:
+# #times in top 50:
+# books_for_each_year = lambda : print('These are the top books for each year:')
+# #year:
+#name:
+#author
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+run()
